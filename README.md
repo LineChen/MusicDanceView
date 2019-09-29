8a72af9b46a97aea50a50a680f3cedaec1c7cc01
chenliu <1101587382@qq.com>
2019-09-29 19:18:14 +0800

    feature (view): 测 试 自 定 义 配 置

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MusicDanceView
+
+
+![pic](https://github.com/LineChen/MusicDanceView/blob/master/music_dance.gif)
--- app/src/main/java/com/line/musicdanceview/MainActivity.java
+++ app/src/main/java/com/line/musicdanceview/MainActivity.java
@@ -1,8 +1,12 @@
 package com.line.musicdanceview;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.line.musicdance.DefaultMusicDanceConfig;
+import com.line.musicdance.MusicDanceView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        MusicDanceView musicDanceView = findViewById(R.id.music_dance);
+        musicDanceView.setMusicDanceConfig(new DefaultMusicDanceConfig() {
+            @Override
+            public float getNoteRadius() {
+                return Resources.getSystem().getDisplayMetrics().density * 0;
+            }
+        });
     }
 }
